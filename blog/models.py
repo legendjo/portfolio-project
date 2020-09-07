7f208d6531508311aba6e4c66c47b7d485ddb7e2
Legend <martjo.expert@gmail.com>
2020-09-07 21:35:37 +0100

    Added Blogpost Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
