5164a55cd3f55e683b0f394a4ff01a770f7b153f
Legend <martjo.expert@gmail.com>
2020-09-09 09:07:13 +0100

    -Homepage,-Bootstrap, -Showjobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+
+# Create your views here. https://docs.djangoproject.com/en/2.2/topics/http/views/
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})  # jobs/home.html since home.html is in /jobs subfolder in templates
